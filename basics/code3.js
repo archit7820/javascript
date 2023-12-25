18e460e89c0e66c8093b2cd42205bd0e2d96fe46
Archit <archit.mymail@gmail.com>
2023-12-25 06:30:52 +0530

    latest commit

---
+++ basics/code3.js
@@ -0,0 +1,19 @@
+
+
+// use strict  is mentioned when we use strictly new standards for javascript ECMA standards 
+// js basically run in browser as well as node js environment 
+
+
+ //  primitive DataTypes ::: 
+let  name  = "Archit"    // string
+let age = 22      // number    range  = 2^53 
+let is_True = false     //  boolean     
+let codingSkill =  undefined      // undefined ==> typeof =  undefined 
+//  here in javascript undefined means not defined value but the variable is already declared   . No value defined 
+
+// symbol ===>>   unique (defining uniqueness )
+
+// null ===>   It is a Datatype  , standalone value  , Null is a type ,  {empty value representation }  , its variable not even declared unlike in  case of undefined .
+console.table([typeof name , typeof age , typeof is_True , typeof codingSkill , null ])
+
+
